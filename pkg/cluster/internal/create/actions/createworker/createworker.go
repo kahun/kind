76bb8b85d4348d29ec7efdd349df17e8d5584c27
Francisco Augusto <faugusto@stratio.com>
2023-08-02 13:31:06 +0200

    Clean credentials

--- pkg/cluster/internal/create/actions/createworker/createworker.go
+++ pkg/cluster/internal/create/actions/createworker/createworker.go
@@ -256,6 +256,7 @@ func (a *action) Execute(ctx *actions.ActionContext) error {
 
 	// Clean keos cluster file
 	keosCluster := a.keosCluster
+	keosCluster.Spec.Credentials = commons.Credentials{}
 	keosCluster.Spec.Keos = struct {
 		Flavour string `yaml:"flavour,omitempty"`
 		Version string `yaml:"version,omitempty"`
