38bd9d2e311af10bccc48d1e4e40e437f526cbb6
Francisco Augusto <faugusto@stratio.com>
2023-08-03 12:52:18 +0200

    Change apply

--- pkg/cluster/internal/create/actions/createworker/createworker.go
+++ pkg/cluster/internal/create/actions/createworker/createworker.go
@@ -315,7 +315,7 @@ app:
 		defer ctx.Status.End(false)
 
 		// Apply cluster manifests
-		c = "kubectl apply -n " + capiClustersNamespace + " -f " + descriptorPath
+		c = "kubectl apply -f " + manifestsPath + "/keoscluster.yaml"
 		_, err = commons.ExecuteCommand(n, c)
 		if err != nil {
 			return errors.Wrap(err, "failed to apply manifests")
