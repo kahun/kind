ad2a7c067ad2f6bfc13e3c307b2cc741e3a7d66f
stg <65890694+stg-0@users.noreply.github.com>
2023-09-12 12:45:12 +0200

    Update pkg/cluster/internal/create/actions/createworker/provider.go

--- pkg/cluster/internal/create/actions/createworker/provider.go
+++ pkg/cluster/internal/create/actions/createworker/provider.go
@@ -244,7 +244,7 @@ func deployClusterOperator(n nodes.Node, keosCluster commons.KeosCluster, cluste
 			keosCluster.Spec.ControlPlane.AWS = commons.AWSCP{}
 		}
 		if keosCluster.Spec.ControlPlane.Managed {
-			// Setting the parameter to false, hide it from the KeosCluster object
+			// Setting the parameter to false, hides it from the KeosCluster object
 			keosCluster.Spec.ControlPlane.HighlyAvailable = false
 		}
 		keosCluster.Spec.Keos = struct {
