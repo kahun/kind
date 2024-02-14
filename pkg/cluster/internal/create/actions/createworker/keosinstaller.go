d364947ef60b50bfd468ccef776124b6dbaa1828
Francisco Augusto <faugusto@stratio.com>
2024-02-14 18:33:06 +0100

    Add keos 1.1 support

--- pkg/cluster/internal/create/actions/createworker/keosinstaller.go
+++ pkg/cluster/internal/create/actions/createworker/keosinstaller.go
@@ -19,6 +19,8 @@ package createworker
 import (
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 
 	"gopkg.in/yaml.v3"
@@ -53,7 +55,7 @@ type KEOSDescriptor struct {
 			Ipip                 bool   `yaml:"ipip,omitempty"`
 			VXLan                bool   `yaml:"vxlan,omitempty"`
 			Pool                 string `yaml:"pool,omitempty"`
-			DeployTigeraOperator bool   `yaml:"deploy_tigera_operator"`
+			DeployTigeraOperator bool   `yaml:"deploy_tigera_operator,omitempty"`
 		} `yaml:"calico"`
 		ClusterID string `yaml:"cluster_id"`
 		Dns       struct {
@@ -143,7 +145,15 @@ func createKEOSDescriptor(keosCluster commons.KeosCluster, storageClass string) 
 			keosDescriptor.Keos.Calico.Pool = "192.168.0.0/16"
 		}
 	}
-	keosDescriptor.Keos.Calico.DeployTigeraOperator = false
+	version := strings.TrimPrefix(keosCluster.Spec.Keos.Version, "v")
+	splitVersion := strings.Split(version, ".")
+	keosVersion, err := strconv.ParseFloat(splitVersion[0]+"."+splitVersion[1], 64)
+	if err != nil {
+		return err
+	}
+	if keosVersion > 1 {
+		keosDescriptor.Keos.Calico.DeployTigeraOperator = false
+	}
 
 	// Keos - Storage
 	keosDescriptor.Keos.Storage.DefaultStorageClass = storageClass
